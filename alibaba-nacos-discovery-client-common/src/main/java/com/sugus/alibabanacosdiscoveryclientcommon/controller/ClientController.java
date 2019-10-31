b44a6f5d149323d5e81a3bc6cc69ba15dfc17c6b
suguswong <suguswong@outlook.com>
2019-10-31 18:20:13 +0800

    增加动态配置获取参数

--- alibaba-nacos-discovery-client-common/src/main/java/com/sugus/alibabanacosdiscoveryclientcommon/controller/ClientController.java
+++ alibaba-nacos-discovery-client-common/src/main/java/com/sugus/alibabanacosdiscoveryclientcommon/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.sugus.alibabanacosdiscoveryclientcommon.controller;
 import com.sugus.alibabanacosdiscoveryclientcommon.service.ServerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,12 @@ public class ClientController {
     /*@Autowired
     private LoadBalancerClient loadBalancerClient;*/
 
+    @Value("${actuator-default:}")
+    private String actuatorDefault;
+
+    @Value("${log-default:}")
+    private String logDefault;
+
     @Autowired
     private ServerService serverService;
 
@@ -29,7 +36,7 @@ public class ClientController {
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);*/
         String result = serverService.hello(name);
-        return "return : " + result;
+        return "return : " + result + ",actuatorDefault:" + actuatorDefault + ",logDefault:" + logDefault;
     }
 
 }
